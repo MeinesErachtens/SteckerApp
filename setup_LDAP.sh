4e2a7e6d726360638206fb5aebbf54a711e9510d
MeinesErachtens <s143527@hft-leipzig.de>
2016-01-03 10:38:28 +0100

    Update setup_LDAP.sh

--- setup_LDAP.sh
+++ setup_LDAP.sh
@@ -3,4 +3,4 @@
 run_cmd sudo usermod -a -G admin frappe
 run_cmd sudo apt-get install python-dev libldap2-dev libsasl2-dev libssl-dev
 run_cmd pip install python-ldap
-run_cmd sudo bench get-app frappe_ldap https://github.com/saurabh6790/frappe_ldap.git
+run_cmd sudo bench get-app frappe_ldap https://github.com/shft117/SteckerApp/tree/master/frappe_ldap-master/frappe_ldap.git
